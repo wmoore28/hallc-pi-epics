7a0263cb1cdbe66885b7d56616e275e493b8feca
Wesley Moore <wmoore@jlab.org>
2017-07-11 12:39:13 -0400

    lakeshore224: switched from usb to ethernet

--- iocBoot/ioclakeshore224/st.cmd
+++ iocBoot/ioclakeshore224/st.cmd
@@ -10,25 +10,16 @@ dbLoadDatabase("dbd/lakeshore224.dbd")
 lakeshore224_registerRecordDeviceDriver(pdbbase)
 
 ## Port config
-drvAsynSerialPortConfigure("USB0", "/dev/ttyUSB0",0,0,0)
-asynSetOption("USB0", -1, "baud", "57600")
-asynSetOption("USB0", -1, "bits", "7")
-asynSetOption("USB0", -1, "stop", "1")
-asynSetOption("USB0", -1, "parity", "odd")
-asynSetOption("USB0", -1, "clocal", "Y")
-asynSetOption("USB0", -1, "crtscts", "N")
-asynOctetSetOutputEos("USB0", 0, "\n")
-asynOctetSetInputEos("USB0", 0, "\n")
+drvAsynIPPortConfigure("ETH0","169.254.7.12:7777",0,0,0)
 
 ## Port debugging
-asynSetTraceMask("USB0", -1, 0x9)
-asynSetTraceIOMask("USB0", -1, 0x2)
+#asynSetTraceMask("ETH0", -1, 0x9)
+#asynSetTraceIOMask("ETH0", -1, 0x2)
 
 ## Load record instances
 #dbLoadRecords("${DEVIOCSTATS}/db/iocAdminSoft.db", "IOC=${IOC}")
 #dbLoadRecords("${AUTOSAVE}/asApp/Db/save_restoreStatus.db", "P=${IOC}:")
-dbLoadRecords("db/lakeshore224.db", "P=C_LASERDAQ, R=TEMP:, PORT=USB0"
-
+dbLoadRecords("db/lakeshore224.db", "P=C_LASERDAQ, R=TEMP:, PORT=ETH0"
 
 cd "${TOP}/iocBoot/${IOC}"
 dbl > pv.list
