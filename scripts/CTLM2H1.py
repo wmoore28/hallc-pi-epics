742ad591cb5ff1b68a7f9b0b6cfc3b09d06acf73
Wesley Moore <wmoore@jlab.org>
2017-10-06 11:56:06 -0400

    CTLM2H1.py: updated for speed test

--- scripts/CTLM2H1.py
+++ scripts/CTLM2H1.py
@@ -5,14 +5,17 @@ import datetime
 import serial
 #import epics
 
+#record_pv = epics.PV("C_LASER:DAQ:REC")        # 0=off, 1=on
+#speed_pv  = epics.PV("C_LASER:DAQ:SPD")        # 0=low, 1=high
+
 ser=serial.Serial('/dev/ttyUSB0',
                   baudrate=9600,
                   parity=serial.PARITY_NONE,
                   stopbits=serial.STOPBITS_ONE,
                   bytesize=serial.EIGHTBITS,
                   timeout=2
-                
                   )
+
 print(ser.isOpen) # checking if serial port is open
 counter=0
 counter1=0
@@ -22,25 +25,26 @@ data2="\x25"
 data3="\xA5\x00"
 data4="\x01"
 #ser.close()
-f=open('/home/pi/Desktop/Python_code/CTLM2H1_2_slow','w')
+f=open('/home/pi/Desktop/CTLM2H1_2_slow','w')
+#f=open('/data/CTLM2H1_slow','w')
 start = time.time()
 #print start
-#while (time.time() - start) <= 3.0:
-#while counter <= 16:
-while True:
-    #ser.open()
-    ser.write(data1)# query
-    counter +=1
-    s=ser.read(2)# reading response 2 bytes
-    y=datetime.datetime.now()
-    #if counter1 > 300:
-    #    print s
-    #    counter1=0
-    x=(int(s.encode('hex'),16)-1000)/10# converting to hex
-    #print s.encode('hex')
-    f.write("%s %s\n" %(x,y))# writing to file
-    #print counter
-    time.sleep(0.2)
+while (time.time() - start) <= 3.0:
+#while counter <= 3:
+#while True:
+#    if record_pv.value:
+    	ser.write(data1)# query
+    	counter +=1
+    	s=ser.read(2)# reading response 2 bytes
+    	y=datetime.datetime.now()
+    	#if counter1 > 300:
+    	#    print s
+    	#    counter1=0
+    	x=(int(s.encode('hex'),16)-1000)/10# converting to hex
+    	#print s.encode('hex')
+    	f.write("%s %s\n" %(x,y))# writing to file
+    	#print counter
+    	#time.sleep(0.2)
 print counter
 ser.close()
 f.close()
